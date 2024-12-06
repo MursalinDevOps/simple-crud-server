5f3208678572f0691c1addae0c5dae25fc81e06e
MursalinDevOps <mursalin1768@gmail.com>
2024-12-07 00:05:11 +0600

    delete method added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -30,12 +30,11 @@ async function run() {
     const database = client.db("usersDB");
     const userCollection = database.collection("users");
 
-    app.get('/users', async(req, res) => {
+    app.get("/users", async (req, res) => {
       const cursor = userCollection.find();
       const result = await cursor.toArray();
       res.send(result);
-    })
-
+    });
 
     app.post("/users", async (req, res) => {
       const user = req.body;
@@ -44,9 +43,12 @@ async function run() {
       res.send(result);
     });
 
-    app.delete('/users/:id', (req, res) => {
+    app.delete("/users/:id", async(req, res) => {
       const id = req.params.id;
       console.log("Deleting user with ID:", id);
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
     });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
